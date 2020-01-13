acd20bb92b5b6974726c2a32a3a6c482ead5b89c
VinayModepalli <vinaykumarmodepalli@gmail.com>
2020-01-14 03:28:23 +0530

    Updates

---
+++ precache-manifest.d947abdf8877f0748b1ca8e05ce50fc5.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f8464be74cb5739ebcb2e395f4cb0c68",
+    "url": "/todoapp/index.html"
+  },
+  {
+    "revision": "ea49ffe1a5f2e3143c7d",
+    "url": "/todoapp/static/css/main.5ecd60fb.chunk.css"
+  },
+  {
+    "revision": "633b3d1018f58e717dfc",
+    "url": "/todoapp/static/js/2.d46d8f21.chunk.js"
+  },
+  {
+    "revision": "d705cb622423d72c5defbf368ca70dcc",
+    "url": "/todoapp/static/js/2.d46d8f21.chunk.js.LICENSE"
+  },
+  {
+    "revision": "ea49ffe1a5f2e3143c7d",
+    "url": "/todoapp/static/js/main.c0dfdb59.chunk.js"
+  },
+  {
+    "revision": "63ed8d88c0187d779e28",
+    "url": "/todoapp/static/js/runtime-main.47b19ebb.js"
+  }
+]);
